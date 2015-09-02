4888096e8e753810d8416492a298a0ddc9709665
Manu <ebeloin@hotmail.com>
2015-09-02 15:00:55 -0400

    y4y4y45

--- app/src/main/java/com/example/manu/exercice1/MainActivity.java
+++ app/src/main/java/com/example/manu/exercice1/MainActivity.java
@@ -1,11 +1,62 @@
 package com.example.manu.exercice1;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int mcouleur = Color.RED;
+    public void button1click(View view)
+    {
+        ToggleButton tog1 = (ToggleButton) findViewById(R.id.toggleButton);
+        TextView couleurPanel = (TextView) findViewById(R.id.zone_couleur);
+        if(tog1.isChecked())
+        {
+            mcouleur = mcouleur | 0x00FF0000;
+        }
+        else {
+            mcouleur = mcouleur & 0xFF00FFFF;
+        }
+        couleurPanel.setBackgroundColor(mcouleur);
+    }
+
+    public void button2click(View view)
+    {
+        ToggleButton tog2 = (ToggleButton) findViewById(R.id.toggleButton2);
+        TextView couleurPanel = (TextView) findViewById(R.id.zone_couleur);
+        if(tog2.isChecked())
+        {
+            mcouleur = mcouleur | 0x0000FF00;
+        }
+        else
+        {
+            mcouleur = mcouleur & 0xFFFF00FF;
+        }
+        couleurPanel.setBackgroundColor(mcouleur);
+    }
+
+    public void button3click(View view)
+    {
+        ToggleButton tog3 = (ToggleButton) findViewById(R.id.toggleButton3);
+        TextView couleurPanel = (TextView) findViewById(R.id.zone_couleur);
+        if(tog3.isChecked())
+        {
+            mcouleur = mcouleur | 0x000000FF;
+        }
+        else
+        {
+            mcouleur = mcouleur & 0xFFFFFF00;
+        }
+        couleurPanel.setBackgroundColor(mcouleur);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
